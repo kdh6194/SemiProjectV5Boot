508209781a3914f99d628ba5aa7e0dd7f08caf98
kdh6194 <kdh6194@naver.com>
2023-05-11 12:57:38 +0900

    저장소 초기화 - boot 프로젝트 설정

---
+++ src/main/java/honeybee/project/semiprojectv5/SemiProjectv5Application.java
@@ -0,0 +1,13 @@
+package honeybee.project.semiprojectv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectv5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProjectv5Application.class, args);
+    }
+
+}
